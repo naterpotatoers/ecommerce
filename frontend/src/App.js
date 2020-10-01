53f85e65cd144eabe5b7338cf2174535221e58c1
naterpotatoers <nate.garza@hotmail.com>
2020-09-30 18:48:21 -0700

    Adds bootstrap and header/footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./screens/Home";
 
 function App() {
   return (
     <>
-      <h1>Welcome to Nateslist</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to Nateslist</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
