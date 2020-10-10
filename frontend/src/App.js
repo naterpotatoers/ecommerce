b964f30ea1ca32dbe5c274d62743607de5f046ad
naterpotatoers <nate.garza@hotmail.com>
2020-10-09 20:51:29 -0700

    adds homescreen placeholders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./screens/Home";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <main>
         <Container>
-          <h1>Welcome to Nateslist</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
