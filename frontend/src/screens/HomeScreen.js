dbdc6cf211896e7d7595925421cdbcc1b94095ba
naterpotatoers <nate.garza@hotmail.com>
2020-10-24 17:27:42 -0700

    adds express server and connects to frontend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
+import axios from "axios";
+
 import Product from "../components/Product";
 
-import products from "../products";
-
 const HomeScreen = () => {
+  // useState for functional component since no constructor like Component
+  const [products, setProducts] = useState([]);
+
+  useEffect(
+    () => {
+      const fetchProducts = async () => {
+        const res = await axios.get("/api/products");
+        setProducts(res.data);
+      };
+
+      fetchProducts();
+    },
+    [] /* put in things that might be changed - for now empty */
+  );
   return (
     <>
       <h1>Latest Products</h1>
