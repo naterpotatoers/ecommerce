494d0dcddf241de2578ad54748e8df9203e0a711
naterpotatoers <nate.garza@hotmail.com>
2020-10-29 22:45:31 -0700

    adds mongoose/mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -23,7 +28,8 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(
+  console.info(
     `Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`
+      .yellow.bold
   )
 );
